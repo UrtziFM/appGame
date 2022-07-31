9a9dc1c6e05b0fe0fca391b89a9b23b520adac26
u100247 <u100247@MM0012B7.local>
2022-07-31 21:14:50 +0200

    Skills component is done, let's go with routes

--- appGame/src/app/app.module.ts
+++ appGame/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RrssComponent } from './components/rrss/rrss.component';
 import { SkillsComponent } from './skills/skills.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { SkillsComponent } from './skills/skills.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
