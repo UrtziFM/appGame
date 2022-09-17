dc1a2a90359b8124c2f8544087fd613eb5337184
u100247 <u100247@MM0012B7.local>
2022-09-17 20:38:22 +0200

    the SPA is done, I will link games now

--- appGame/src/app/app-routing.module.ts
+++ appGame/src/app/app-routing.module.ts
@@ -2,14 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-import { SpacesComponent } from './spaces/spaces.component';
-import { PacmanComponent } from './pacman/pacman.component';
-import { BreakoutComponent } from './breakout/breakout.component';
-
 const routes: Routes = [
-  {path: '', component: SpacesComponent},
-  {path: 'Pacman', component: PacmanComponent},
-  {path: 'Breakout', component: BreakoutComponent}
 ];
 
 @NgModule({
--- appGame/src/app/app.module.ts
+++ appGame/src/app/app.module.ts
@@ -7,19 +7,13 @@ import { RrssComponent } from './components/rrss/rrss.component';
 import { SkillsComponent } from './skills/skills.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { GamesComponent } from './games/games.component';
-import { SpacesComponent } from './spaces/spaces.component';
-import { PacmanComponent } from './pacman/pacman.component';
-import { BreakoutComponent } from './breakout/breakout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RrssComponent,
     SkillsComponent,
-    GamesComponent,
-    SpacesComponent,
-    PacmanComponent,
-    BreakoutComponent
+    GamesComponent
   ],
   imports: [
     BrowserModule,
